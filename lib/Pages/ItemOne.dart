2522f46b671dbb07188d77c4de595e1cc7f6a9cf
Yazdani <yaz4dani@gmail.com>
2020-01-13 22:55:21 +0600

    Grid View

--- lib/Pages/ItemOne.dart
+++ lib/Pages/ItemOne.dart
@@ -204,9 +204,7 @@ class _ItemOneState extends State<ItemOne> {
                       ),
                       ),
                     ),
-
                     SizedBox(height: 6.0,),
-
                     Container(
                       padding: EdgeInsets.all(10.0),
                       child: Text(des,
